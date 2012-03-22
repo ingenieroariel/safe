adb179a7cb9bab0e387024e08d36c7f9a6518081
Ariel Núñez <ingenieroariel@gmail.com>
2012-03-22 15:44:46 -0400

    Added project skeleton

---
+++ setup.py
@@ -0,0 +1,33 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Get text from README.txt
+readme_text = file('README.rst', 'rb').read()
+
+setup(name          = 'SAFE',
+      version       = '0.1',
+      description   = "Geospatial impact engine",
+      license       = 'BSD',
+      keywords      = 'gis vector feature raster data',
+      author        = 'Ole Nielsen',
+      author_email  = 'ole.moller.nielsen@gmail.com',
+      maintainer        = 'Ariel Núñez',
+      maintainer_email  = 'ingenieroariel@gmail.com',
+      url   = 'http://github.com/AIFDR/safe',
+      long_description = readme_text,
+      package_dir = {'': ''},
+      packages = ['safe'],
+      install_requires  = [],
+      tests_require = ['nose'],
+      classifiers   = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS',
+        ],
+)
